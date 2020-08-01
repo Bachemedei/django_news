4dab4de11eec428b939a65eabb0e4e3054acb96d
Bachemedei <bronwyn.1992@gmail.com>
2020-08-01 12:45:59 +0800

    finished saturday class. Applied some CSS

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,18 +1,31 @@
 from django import forms
 from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 from .models import NewsStory
+
 
 class StoryForm(ModelForm):
     class Meta:
         model = NewsStory
-        fields = ['title', 'author', 'pub_date', 'content']
+        fields = ['title', 'author', 'pub_date', 'content', 'image']
+        labels = {
+            'image' : _('Image (URL)'),
+            'pub_date' : _('Publication Date')
+        }
         widgets = {
-            'pub_date': forms.DateInput(
-                format=('%m/%d/%Y'),
+            'title': forms.TextInput(attrs={
+                'class': 'title-input',
+                'id' : 'title-label'
+                }),
+            'author': forms.TextInput(attrs={'class': 'author-input'}),
+            'pub_date': forms.DateTimeInput(
+                format=('%Y-%m-%d %H:%M'),
                 attrs={
                     'class': 'form-control',
                     'placeholder': 'Select a date',
                     'type': 'date'
                 }
-            )
+            ),
+            'content': forms.Textarea(attrs={'class': 'content-input'}),
+            'image': forms.URLInput(attrs={'class': 'image-input'})
         }
