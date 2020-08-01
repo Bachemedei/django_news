bd814d54fe60862b56e883c95d4ad4019408c0c1
Bachemedei <bronwyn.1992@gmail.com>
2020-08-01 14:43:13 +0800

    Finished part 1

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,31 +1,21 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, SplitDateTimeField, SplitDateTimeWidget
 from django.utils.translation import ugettext_lazy as _
+
 from .models import NewsStory
 
 
 class StoryForm(ModelForm):
+    pub_date = SplitDateTimeField(
+        widget = SplitDateTimeWidget(
+            date_attrs={'type': 'date'},
+            time_attrs={'type': 'time'},
+        )   
+    )
     class Meta:
         model = NewsStory
         fields = ['title', 'author', 'pub_date', 'content', 'image']
         labels = {
             'image' : _('Image (URL)'),
-            'pub_date' : _('Publication Date')
         }
-        widgets = {
-            'title': forms.TextInput(attrs={
-                'class': 'title-input',
-                'id' : 'title-label'
-                }),
-            'author': forms.TextInput(attrs={'class': 'author-input'}),
-            'pub_date': forms.DateTimeInput(
-                format=('%Y-%m-%d %H:%M'),
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Select a date',
-                    'type': 'date'
-                }
-            ),
-            'content': forms.Textarea(attrs={'class': 'content-input'}),
-            'image': forms.URLInput(attrs={'class': 'image-input'})
-        }
+
